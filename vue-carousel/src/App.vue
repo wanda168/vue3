85a4e9fac71df6035f087e3f90ebd1220c63c771
wanda <vdking.168@gmail.com>
2022-11-28 23:43:29 +0700

    slide component

--- vue-carousel/src/App.vue
+++ vue-carousel/src/App.vue
@@ -1,68 +1,21 @@
 <template>
-  <div class="btns">
-    <button @click="showTopLeftToast = true">Show Top Left Toast</button>
-    <button @click="showTopRightToast = true">Show Top Right Toast</button>
-    <button @click="showBottomLeftToast = true">Show Bottom Left Toast</button>
-    <button @click="showBottomRightToast = true">Show Bottom Right Toast</button>
-  </div>
-  <Toast
-    message="Message has been sent"
-    :show="showTopLeftToast"
-    @hide="showTopLeftToast = false"
-    type="success"
-    position="top-left"
-  />
-  <Toast
-    message="Message has been sent"
-    :show="showTopRightToast"
-    @hide="showTopRightToast = false"
-    type="warning"
-    position="top-right"
-  />
-  <Toast
-    message="Message has been sent"
-    :show="showBottomLeftToast"
-    @hide="showBottomLeftToast = false"
-    type="error"
-    position="bottom-left"
-  />
-  <Toast
-    message="Message has been sent"
-    :show="showBottomRightToast"
-    @hide="showBottomRightToast = false"
-    type="success"
-    position="bottom-right"
-  />
+  <carousel :slides="slides"></carousel>
 </template>
 
 <script>
-import Toast from "./components/toast/Toast.vue";
+import Carousel from "./components/carousel/Carousel.vue";
 export default {
-  components: { Toast },
+  components: { Carousel },
   data: () => ({
-      showTopLeftToast: false,
-      showTopRightToast: false,
-      showBottomLeftToast: false,
-      showBottomRightToast: false,
+    slides: [
+      "https://picsum.photos/id/1032/900/400",
+      "https://picsum.photos/id/1033/900/400",
+      "https://picsum.photos/id/1035/900/400",
+      "https://picsum.photos/id/1036/900/400",
+      "https://picsum.photos/id/1037/900/400",
+    ],
   }),
 };
 </script>
 
-<style>
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-
-.btns {
-  margin: 10px 15%;
-  display: flex;
-  padding: 15% 10px;
-  justify-content: space-between;
-}
-
-.btns button:hover {
-  cursor: pointer;
-}
-
-</style>
+<style></style>
